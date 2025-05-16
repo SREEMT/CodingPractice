c0599e2409324b49bb547f799c12a70aad7fb28e
SREEMT <tmeers101@gmail.com>
2025-05-16 08:02:58 -0400

    added time_track and time_format functions to track time lapsed

--- PythonPractice/Minesweeper/MinesweeperTkinter.py
+++ PythonPractice/Minesweeper/MinesweeperTkinter.py
@@ -7,8 +7,11 @@ Minesweeper program using Tkinter for the GUI.
 Proogram is meant for coding practice.
 '''
 
+import time
 import tkinter as tk
 
+# Tkinter Test
+'''
 window = tk.Tk()
 window.title("Hello World")
 
@@ -23,3 +26,34 @@ button.pack()
 
 # Start the event loop.
 window.mainloop()
+'''
+
+def time_format(sec):
+    mins = sec // 60
+    sec = sec % 60
+    hours = mins // 60
+    mins = mins % 60
+
+    print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),int(sec)))
+
+def time_track():
+
+    # Test condition, value will change to true if game ends stopping the timer
+    game_end = False
+
+    start_time = time.time()
+    prev_sec = -1
+
+    while not game_end:
+        end_time = time.time()
+        time_lapsed = end_time - start_time
+        curr_sec = int(time_lapsed)
+
+        if curr_sec != prev_sec:
+            prev_sec = curr_sec
+            time_format(curr_sec)
+
+def main():
+    time_track()
+
+main()
